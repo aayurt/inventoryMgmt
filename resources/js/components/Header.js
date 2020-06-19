fb7d3e95a0bfe1982057ca7960c99754f04d59d9
aayurt <ayurtshrestha@gmail.com>
2020-06-19 19:53:35 +0545

    changes for dropdown

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -1,36 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import {Button} from '@material-ui/core';
-import * as actions from '../store/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button, Menu, MenuItem } from "@material-ui/core";
+import * as actions from "../store/actions";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+const Header=(props)=>{
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-class Header extends Component {
-  handleLogout = (e) => {
-    e.preventDefault();
-    this.props.dispatch(actions.authLogout());
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  render() {
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleLogout = e => {
+    e.preventDefault();
+    props.dispatch(actions.authLogout());
+    setAnchorEl(null);
+
+  }
     return (
       <header className="d-flex align-items-center justify-content-between">
         <h1 className="logo my-0 font-weight-normal h4">
-          <Link to="/">Inventory Management</Link>
+          <Link to="/">Inventory Mansment</Link>
         </h1>
 
-        {this.props.isAuthenticated && (
+        {props.isAuthenticated && (
           <div className="navigation d-flex justify-content-end">
-                  <Button onClick={this.handleLogout}>
-                    Log Out
-                  </Button>
+           
+            <ExpandMoreIcon aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}/>
+         
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem>Profile</MenuItem>
+              <MenuItem>My account</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
           </div>
         )}
       </header>
     );
-  }
+  
 }
 
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.Auth.isAuthenticated,
+const mapStateToProps = state => ({
+  isAuthenticated: state.Auth.isAuthenticated
 });
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header)
